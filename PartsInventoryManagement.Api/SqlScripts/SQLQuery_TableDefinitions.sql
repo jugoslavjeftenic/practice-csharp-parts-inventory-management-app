8b82a6843a9fc6f1ac98e8e96aa56d757c0fb96c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-16 16:34:02 +0100

    New table Inventory and changes in SQL scripts

--- PartsInventoryManagement.Api/SqlScripts/SQLQuery_TableDefinitions.sql
+++ PartsInventoryManagement.Api/SqlScripts/SQLQuery_TableDefinitions.sql
@@ -1,15 +1,32 @@
-CREATE DATABASE PartsInventory
+-- CREATE DATABASE PartsInventory
+-- GO
+
+USE PartsInventory;
 GO
 
-USE PartsInventory
+IF OBJECT_ID('dbo.Parts', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Parts;
+END;
+GO
+
+IF OBJECT_ID('dbo.Locations', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Locations;
+END;
+GO
+
+IF OBJECT_ID('dbo.Inventory', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Inventory;
+END;
 GO
 
 CREATE TABLE Parts
 (
     PartId INT IDENTITY(1,1) PRIMARY KEY,
     PartCategory NVARCHAR(50),
-    PartName NVARCHAR(MAX),
-    PartQuantity INT,
+    PartName NVARCHAR(MAX)
 )
 GO
 
@@ -18,6 +35,17 @@ CREATE TABLE Locations
     LocationId INT IDENTITY(1,1) PRIMARY KEY,
     LocationAlpha VARCHAR(5),
     LocationName NVARCHAR(MAX),
-    LocationColor VARCHAR(6),
+    LocationColor VARCHAR(6)
+)
+GO
+
+CREATE TABLE Inventory
+(
+    InventoryId INT IDENTITY(1,1) PRIMARY KEY,
+    PartId INT,
+    LocationId INT,
+    PartQuantity INT,
+    CONSTRAINT FK_Inventory_Parts FOREIGN KEY (PartId) REFERENCES Parts(PartId),
+    CONSTRAINT FK_Inventory_Locations FOREIGN KEY (LocationId) REFERENCES Locations(LocationId)
 )
 GO
--- PartsInventoryManagement.Api/SqlScripts/SQLQuery_TableQueries.sql
+++ PartsInventoryManagement.Api/SqlScripts/SQLQuery_TableQueries.sql
@@ -1,17 +1,25 @@
-SELECT 
+SELECT
     [PartId],
     [PartCategory],
-    [PartName],
-    ISNULL([PartQuantity], 0) AS PartQuantity
+    [PartName]
 FROM dbo.Parts
 ORDER BY PartId DESC
 GO
 
-SELECT 
+SELECT
     [LocationId],
     [LocationAlpha],
     [LocationName],
     [LocationColor]
 FROM dbo.Locations
 ORDER BY LocationId DESC
+GO
+
+SELECT
+    [InventoryId],
+    [PartId],
+    [LocationId],
+    ISNULL([PartQuantity], 0) AS PartQuantity
+FROM dbo.Inventory
+ORDER BY InventoryId DESC
 GO
