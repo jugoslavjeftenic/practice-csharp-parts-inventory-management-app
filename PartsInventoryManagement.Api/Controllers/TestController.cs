49a03fffd70e0dd86b979a18fac1ddb67ba2422a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-16 16:52:03 +0100

    App && Db test controller

---
+++ PartsInventoryManagement.Api/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PartsInventoryManagement.Api.Data;
+using System;
+
+namespace PartsInventoryManagement.Api.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TestController(IConfiguration config) : ControllerBase
+	{
+		private readonly DbContextDapper _dapper = new(config);
+
+		[HttpGet]
+		public string TestApplication()
+		{
+			return "Application is up && running.";
+		}
+
+		[HttpGet("TestDbConnection")]
+		public DateTime TestConnection()
+		{
+			return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+		}
+	}
+}
