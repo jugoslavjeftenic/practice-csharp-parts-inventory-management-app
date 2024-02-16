affef233ba9a8aca03ae2178fbd4bb080afa1a8f
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-16 16:36:52 +0100

    New InventoryModel and change in PartModel

---
+++ PartsInventoryManagement.Api/Models/InventoryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PartsInventoryManagement.Api.Models
+{
+	public class InventoryModel
+	{
+		public int InventoryId { get; set; }
+		public int PartId { get; set; }
+		public string LocationName { get; set; } = string.Empty;
+		public int PartQuantity { get; set; }
+	}
+}
--- PartsInventoryManagement.Api/Models/PartModel.cs
+++ PartsInventoryManagement.Api/Models/PartModel.cs
@@ -5,6 +5,5 @@
 		public int PartId { get; set; }
 		public string PartCategory { get; set; } = string.Empty;
 		public string PartName { get; set; } = string.Empty;
-		public int PartQuantity { get; set; }
 	}
 }
