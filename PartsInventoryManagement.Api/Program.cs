8119a9126f34dc84cd40ec8990425bac60efc400
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-16 15:56:45 +0100

    DependencyInjection && Hosting setup

--- PartsInventoryManagement.Api/Program.cs
+++ PartsInventoryManagement.Api/Program.cs
@@ -1,44 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Cors
+builder.Services.AddCors((options) =>
+{
+	options.AddPolicy("DevCors", (corsBuilder) =>
+	{
+		corsBuilder
+			.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000")
+			.AllowAnyMethod()
+			.AllowAnyHeader()
+			.AllowCredentials();
+	});
+	options.AddPolicy("ProdCors", (corsBuilder) =>
+	{
+		corsBuilder
+			.WithOrigins("https://myProductionSite.com")
+			.AllowAnyMethod()
+			.AllowAnyHeader()
+			.AllowCredentials();
+	});
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+	app.UseCors("DevCors");
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
+else
 {
-	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-	var forecast = Enumerable.Range(1, 5).Select(index =>
-		new WeatherForecast
-		(
-			DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-			Random.Shared.Next(-20, 55),
-			summaries[Random.Shared.Next(summaries.Length)]
-		))
-		.ToArray();
-	return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+	app.UseCors("ProdCors");
+	app.UseHttpsRedirection();
 }
+
+app.MapControllers();
+app.Run();
