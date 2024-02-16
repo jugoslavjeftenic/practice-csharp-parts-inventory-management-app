4ce8c5e99b71e5186c46e0a7fcf06a35326d8f3a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-16 15:43:36 +0100

    Models

---
+++ PartsInventoryManagement.Api/Models/LocationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PartsInventoryManagement.Api.Models
+{
+	public class LocationModel
+	{
+		public int LocationId { get; set; }
+		public string LocationAlpha { get; set; } = string.Empty;
+		public string LocationName { get; set; } = string.Empty;
+		public string LocationColor { get; set; } = string.Empty;
+	}
+}
---
+++ PartsInventoryManagement.Api/Models/PartModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PartsInventoryManagement.Api.Models
+{
+	public class PartModel
+	{
+		public int PartId { get; set; }
+		public string PartCategory { get; set; } = string.Empty;
+		public string PartName { get; set; } = string.Empty;
+		public int PartQuantity { get; set; }
+	}
+}
