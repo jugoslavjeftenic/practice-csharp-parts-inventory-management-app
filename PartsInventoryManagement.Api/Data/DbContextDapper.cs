3d99707399191a3018084a394f2c4f9c4fa0aeaa
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-17 12:38:28 +0100

    Removed methods without of DynamicParameters

--- PartsInventoryManagement.Api/Data/DbContextDapper.cs
+++ PartsInventoryManagement.Api/Data/DbContextDapper.cs
@@ -10,43 +10,13 @@ namespace PartsInventoryManagement.Api.Data
 	{
 		private readonly IConfiguration _config = config;
 
-		public IEnumerable<T> LoadData<T>(string sql)
-		{
-			IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-			return dbConnection.Query<T>(sql);
-		}
-
-		public T LoadDataSingle<T>(string sql)
-		{
-			IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-			return dbConnection.QuerySingle<T>(sql);
-		}
-
-		public IEnumerable<T> LoadDataWithParameters<T>(string sql, DynamicParameters parameters)
+		public IEnumerable<T> QuerySql<T>(string sql, DynamicParameters? parameters = null)
 		{
 			IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
 			return dbConnection.Query<T>(sql, parameters);
 		}
 
-		public T LoadDataSingleWithParameters<T>(string sql, DynamicParameters parameters)
-		{
-			IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-			return dbConnection.QuerySingle<T>(sql, parameters);
-		}
-
-		public bool ExecuteSql(string sql)
-		{
-			IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-			return dbConnection.Execute(sql) > 0;
-		}
-
-		public int ExecuteSqlWithRowCount(string sql)
-		{
-			IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-			return dbConnection.Execute(sql);
-		}
-
-		public bool ExecuteSqlWithParameters(string sql, DynamicParameters parameters)
+		public bool ExecuteSql(string sql, DynamicParameters? parameters = null)
 		{
 			IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
 			return dbConnection.Execute(sql, parameters) > 0;
