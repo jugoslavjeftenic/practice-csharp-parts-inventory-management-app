14fd910e56186b3448a2eb7c3b869d7cd44a0863
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-02-24 16:31:57 +0100

    New SeedParts method

--- PartsInventoryManagement.Api/Controllers/PartsController.cs
+++ PartsInventoryManagement.Api/Controllers/PartsController.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using PartsInventoryManagement.Api.Data;
 using PartsInventoryManagement.Api.Dtos;
 using PartsInventoryManagement.Api.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -273,6 +274,115 @@ namespace PartsInventoryManagement.Api.Controllers
 				_dapper.QuerySql<PartModel>(sqlQuery, sqlParameters);
 
 			return Ok(parts);
+		}
+
+		//Seed
+		[HttpPost("seed")]
+		public IActionResult SeedParts()
+		{
+			int counter = 0;
+			List<NewPartDTO> newPartsList =
+			[
+				new NewPartDTO { PartCategoryId = 1, PartName = "HDD 3.5in 500GB"},
+				new NewPartDTO { PartCategoryId = 1, PartName = "HDD 3.5in 1TB"},
+				new NewPartDTO { PartCategoryId = 1, PartName = "HDD 2.5in 500GB"},
+				new NewPartDTO { PartCategoryId = 1, PartName = "HDD 2.5in 1TB"},
+				new NewPartDTO { PartCategoryId = 1, PartName = "SSD 2.5in 256GB"},
+				new NewPartDTO { PartCategoryId = 1, PartName = "SSD 2.5in 512GB"},
+				new NewPartDTO { PartCategoryId = 1, PartName = "SSD NVME 2280 M.2 256GB"},
+				new NewPartDTO { PartCategoryId = 1, PartName = "SSD NVME 2280 M.2 512GB"},
+				new NewPartDTO { PartCategoryId = 2, PartName = "DIMM DDR3 4GB"},
+				new NewPartDTO { PartCategoryId = 2, PartName = "DIMM DDR3 8GB"},
+				new NewPartDTO { PartCategoryId = 2, PartName = "DIMM DDR4 4GB"},
+				new NewPartDTO { PartCategoryId = 2, PartName = "DIMM DDR4 8GB"},
+				new NewPartDTO { PartCategoryId = 2, PartName = "SODIMM DDR3 4GB"},
+				new NewPartDTO { PartCategoryId = 2, PartName = "SODIMM DDR3 8GB"},
+				new NewPartDTO { PartCategoryId = 2, PartName = "SODIMM DDR4 4GB"},
+				new NewPartDTO { PartCategoryId = 2, PartName = "SODIMM DDR4 8GB"},
+				new NewPartDTO { PartCategoryId = 3, PartName = "Miš USB optički crni"},
+				new NewPartDTO { PartCategoryId = 3, PartName = "Tastatura YU USB crna"},
+				new NewPartDTO { PartCategoryId = 3, PartName = "Ethernet adapter USB3 Gb"},
+				new NewPartDTO { PartCategoryId = 4, PartName = "Svič neupravljivi 8 portni"},
+				new NewPartDTO { PartCategoryId = 4, PartName = "Svič neupravljivi 16 portni"},
+				new NewPartDTO { PartCategoryId = 5, PartName = "U/UTP peč kabel Cat5e 1m"},
+				new NewPartDTO { PartCategoryId = 5, PartName = "U/UTP peč kabel Cat5e 2m"},
+				new NewPartDTO { PartCategoryId = 5, PartName = "U/UTP peč kabel Cat5e 3m"},
+				new NewPartDTO { PartCategoryId = 5, PartName = "U/UTP kotur kabel Cat5e (metar)"},
+				new NewPartDTO { PartCategoryId = 5, PartName = "Konektor RJ45"},
+				new NewPartDTO { PartCategoryId = 5, PartName = "Kapica za konektor RJ45"}
+			];
+
+			foreach (var newPart in newPartsList)
+			{
+				DynamicParameters sqlParameters = new();
+				sqlParameters.Add("@PartNameParam", newPart.PartName, DbType.String);
+
+				// Query parts
+				string sqlQueryParts = @$"
+				SELECT *
+				FROM [dbo].[Parts]
+				WHERE [PartName] = @PartNameParam
+				";
+
+				IEnumerable<PartModel> parts =
+					_dapper.QuerySql<PartModel>(sqlQueryParts, sqlParameters);
+
+				if (parts.Any())
+				{
+					continue;
+				}
+
+				// Query part categories
+				string partCategoryName = newPart.PartCategoryId switch
+				{
+					1 => "Diskovi",
+					2 => "Memorije",
+					3 => "Periferije",
+					4 => "Svičevi",
+					5 => "Kablovi",
+					_ => String.Empty,
+				};
+
+				sqlParameters.Add("@PartCategoryNameParam", partCategoryName, DbType.String);
+
+				string sqlQueryPartCategories = @$"
+				SELECT *
+				FROM [dbo].[PartCategories]
+				WHERE [PartCategoryName] = @PartCategoryNameParam
+				";
+
+				IEnumerable<PartCategoryModel> partCategories =
+					_dapper.QuerySql<PartCategoryModel>(sqlQueryPartCategories, sqlParameters);
+
+				if (partCategories.Any() is not true)
+				{
+					continue;
+				}
+
+				sqlParameters.Add("@PartCategoryIdParam", partCategories.First().PartCategoryId, DbType.Int32);
+
+				// Insert part category
+				string sqlExecute = @$"
+				INSERT INTO [dbo].[Parts] (
+					[PartCategoryId],
+					[PartName]
+				) VALUES (
+					@PartCategoryIdParam,
+					@PartNameParam
+				)";
+				try
+				{
+					_dapper.ExecuteSql(sqlExecute, sqlParameters);
+					counter++;
+				}
+				catch (Exception)
+				{
+					throw;
+					//continue;
+				}
+			}
+
+			return Ok($"Dodavanje podataka završeno. Ukupno dodato: {counter}");
 		}
 	}
 }
